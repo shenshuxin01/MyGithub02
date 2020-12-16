d1cb8a6466825147e40de2c5752e90a1ff84e4de
shenshuxin <ssx17852015601@163.com>
2020-12-16 22:09:50 +0800

    Initial commit

---
+++ src/main/java/com/Demo.java
@@ -0,0 +1,10 @@
+package com;
+
+import org.apache.ibatis.io.Resources;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("my sconed project");
+//        Resources.getResourceAsStream()
+    }
+}
